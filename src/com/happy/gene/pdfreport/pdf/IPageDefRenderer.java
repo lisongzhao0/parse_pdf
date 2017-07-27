a2cf50eea227e757b514c1590a89aad2eeacb465
lisongzhao0 <lisongzhao0@msn.cn>
2017-07-27 12:59:47 +0800

    add catalog interface to PageDefRenderer

--- src/com/happy/gene/pdfreport/pdf/IPageDefRenderer.java
+++ src/com/happy/gene/pdfreport/pdf/IPageDefRenderer.java
@@ -3,10 +3,13 @@ package com.happy.gene.pdfreport.pdf;
 import com.happy.gene.pdfreport.pdf.def.PageDef;
 import com.itextpdf.layout.Document;
 
+import java.util.Map;
+
 /**
  * Created by zhaolisong on 10/05/2017.
  */
 public interface IPageDefRenderer {
+    Map<String, Integer> getCatalogs();
     void beforeRenderer(Document pdf, PageDef page) throws Exception;
     void rendererHeader(Document pdf, PageDef page) throws Exception;
     void rendererTail(Document pdf, PageDef page) throws Exception;
